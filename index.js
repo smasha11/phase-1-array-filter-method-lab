033500e1a21c384797782313298bfc8bc2457bbd
smasha11 <ismail.hussein.1120@gmail.com>
2024-05-28 12:04:57 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+// Function to return all drivers that match the passed in name
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  // Function to return drivers if beginning provided letters match
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  
+  // Function to check if name matches within a data structure
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  
